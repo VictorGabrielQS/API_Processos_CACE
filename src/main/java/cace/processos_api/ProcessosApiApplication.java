ea49293284df3fd54ff0e93ff836f1515794a07c
VictorGabrielQS <victor.git24@gmail.com>
2025-05-09 15:22:58 -0300

    Alteração no ProcessosApiApplication

--- src/main/java/cace/processos_api/ProcessosApiApplication.java
+++ src/main/java/cace/processos_api/ProcessosApiApplication.java
@@ -1,5 +1,7 @@
 package cace.processos_api;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -10,8 +12,17 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaRepositories(basePackages = "cace.processos_api.repository")
 public class ProcessosApiApplication {
 
+	@Value("${spring.datasource.url:NOT_DEFINED}")
+	private String dbUrl;
+
 	public static void main(String[] args) {
 		SpringApplication.run(ProcessosApiApplication.class, args);
 	}
 
+	@PostConstruct
+	public void logEnv() {
+		System.out.println("==== DATABASE_URL ====");
+		System.out.println("spring.datasource.url: " + dbUrl);
+		System.out.println("======================");
+	}
 }
