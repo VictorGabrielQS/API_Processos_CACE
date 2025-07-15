40cc319bc7d8881e820ca3f73ad3cb61846691c6
VictorGabrielQS <victor.git24@gmail.com>
2025-07-15 09:34:51 -0300

    redis configurado

--- src/main/java/cace/processos_api/config/RedisConfig.java
+++ src/main/java/cace/processos_api/config/RedisConfig.java
@@ -1,22 +1,46 @@
 package cace.processos_api.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 
+import java.net.URI;
 import java.time.Duration;
 
 @Configuration
 public class RedisConfig {
 
+    @Value("${redis.url}")
+    private String redisUrl;
+
     @Bean
-    public RedisCacheManager redisCacheManager(RedisConnectionFactory connectionFactory) {
+    public RedisConnectionFactory redisConnectionFactory() {
+        URI uri = URI.create(redisUrl); // redis://default:PASSWORD@HOST:PORT
+
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+        config.setHostName(uri.getHost());
+        config.setPort(uri.getPort());
+
+        String[] userInfo = uri.getUserInfo().split(":");
+        if (userInfo.length == 2) {
+            config.setPassword(userInfo[1]); // senha
+        }
+
+        return new LettuceConnectionFactory(config);
+    }
+
+    @Bean
+    public RedisCacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
         RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
-                .entryTtl(Duration.ofMinutes(10)); // Expira em 10 minutos
-        return RedisCacheManager.builder(connectionFactory).cacheDefaults(config).build();
+                .entryTtl(Duration.ofMinutes(10)); // TTL de 10 minutos
+
+        return RedisCacheManager.builder(redisConnectionFactory)
+                .cacheDefaults(config)
+                .build();
     }
 }
-
-
