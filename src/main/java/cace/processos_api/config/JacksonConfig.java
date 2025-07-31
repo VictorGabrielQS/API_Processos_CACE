1c7c7f2bd0acf98bd68c3602238d95721155ac22
VictorGabrielQS <victor.git24@gmail.com>
2025-07-31 10:29:04 -0300

    jsr310

---
+++ src/main/java/cace/processos_api/config/JacksonConfig.java
@@ -0,0 +1,23 @@
+package cace.processos_api.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Configuration;
+
+import jakarta.annotation.PostConstruct;
+
+
+@Configuration
+public class JacksonConfig {
+
+    private final ObjectMapper objectMapper;
+
+    public JacksonConfig(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
+    @PostConstruct
+    public void setUp() {
+        objectMapper.registerModule(new JavaTimeModule());
+    }
+}
