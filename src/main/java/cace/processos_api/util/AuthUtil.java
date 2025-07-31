2495ed71c4f91102a3f3436bdb12e7c84cf3bdde
VictorGabrielQS <victor.git24@gmail.com>
2025-07-31 11:57:02 -0300

    Resolvido erro de rate limit 2

--- src/main/java/cace/processos_api/util/AuthUtil.java
+++ src/main/java/cace/processos_api/util/AuthUtil.java
@@ -1,40 +1,49 @@
 package cace.processos_api.util;
 
 import cace.processos_api.model.process.Usuario;
+import cace.processos_api.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AuthUtil {
+
+    private static UsuarioRepository usuarioRepository;
+
+    @Autowired
+    public AuthUtil(UsuarioRepository usuarioRepository) {
+        AuthUtil.usuarioRepository = usuarioRepository;
+    }
+
     public static Usuario getUsuarioLogado() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if (authentication == null || !authentication.isAuthenticated()) {
             return null;
         }
+
         Object principal = authentication.getPrincipal();
+
         if (principal instanceof Usuario) {
             return (Usuario) principal;
+        } else if (principal instanceof UserDetails) {
+            String username = ((UserDetails) principal).getUsername();
+            return usuarioRepository.findByUsername(username).orElse(null);
         }
+
         return null;
     }
 
+    public static void validarAcesso(Integer... nivelRequerido) {
+        /*
+            DESCRIÇÃO DOS NÍVEIS DE ACESSO
 
-
-    public  static  void validarAcesso(Integer... nivelRequerido){
-
-
-        /*                DESCRIÇÃO DOS NÍVEIS DE ACESSO
-
-        Nivel 1 - acesso total ✅
-
-        Nivel 2 - acesso de usuario , possui limitações , não tem acesso as opções de ADM  👤
-
-        Nivel 3 - sem acesso a nada , cadastro inicial até o usuario trocar a senha
-
-
+            Nivel 1 - acesso total ✅
+            Nivel 2 - acesso de usuario, possui limitações, não tem acesso às opções de ADM 👤
+            Nivel 3 - sem acesso a nada, cadastro inicial até o usuário trocar a senha
         */
-
 
         Usuario usuario = getUsuarioLogado();
         if (usuario == null) {
@@ -44,6 +53,7 @@ public class AuthUtil {
         for (Integer nivel : nivelRequerido) {
             if (usuario.getNivelAcesso() == nivel) return;
         }
+
         throw new RuntimeException("Acesso negado: nível de acesso insuficiente.");
     }
 }
