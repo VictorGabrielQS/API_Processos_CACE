0ae8ce2ef8e0244cba6b3eff6ca0a17219812109
VictorGabrielQS <victor.git24@gmail.com>
2025-09-26 13:55:56 -0300

    Inserido Varas na API - 2

--- src/main/java/cace/processos_api/controller/deltaSap/VaraController.java
+++ src/main/java/cace/processos_api/controller/deltaSap/VaraController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("/varas")
 @RequiredArgsConstructor
 public class VaraController {
     private final VaraService varaService;
